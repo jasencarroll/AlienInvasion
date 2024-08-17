c705b45a8d086795eb76005670b28ffa74d5ae04
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-17 09:24:19 -0400

    refactored to settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,11 @@
 
 """ Building a game named ALien Invasion!
 """
-
 import sys
 
 import pygame
+
+from settings import Settings
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior.
@@ -14,12 +15,11 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # Start the main loop for the game
@@ -30,8 +30,7 @@ class AlienInvasion:
                     sys.exit()
             
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
