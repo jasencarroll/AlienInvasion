0c3d20b94625f2f03766466d4cd22efc829f177e
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-18 09:15:38 -0400

    stored bullets in a group

--- bullet.py
+++ bullet.py
@@ -12,9 +12,21 @@ class Bullet(Sprite):
         self.color = self.settings.bullet_color
 
         # create a bullet rect at (0, 0) and then set correct position. 
-        self.rect = pygameRect(0, 0, self.settings.bullet_width, 
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, 
                                self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         # store the bullet's position as a float. 
         self.y = float(self.rect.y)
+
+    def update(self):
+        '''move the bullet up the screen.'''
+        # update the exact position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # update the rect position. 
+        self.rect.y = self.y
+
+    def draw_bullets(self):
+        '''draw the bullet to the screen'''
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        
