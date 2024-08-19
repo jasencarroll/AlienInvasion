e8d9b1e98c25327ee99b81f5f15df3a66cced631
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-19 16:56:19 -0400

    limited active bullet number

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -62,6 +62,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _update_bullets(self):
+        '''Update position of bullets and get rid of old bullets.'''
+        # Update bullet positions. 
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         '''Redraw the screen during each pass through the loop.'''
         self.screen.fill(self.settings.bg_color)
@@ -73,8 +83,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         '''create a new bullet and add it to the bullets group.'''
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
