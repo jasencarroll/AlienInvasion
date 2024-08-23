e84fd94c24aff5b0972e8a69ab9472ecc3fef044
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-23 19:11:55 -0400

    display play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -189,6 +189,11 @@ class AlienInvasion:
             bullet.draw_bullets()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+        
+        # Draw the play button if the game is inactive. 
+        if not self.game_active:
+            self.play_button.draw_button() 
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
--- button.py
+++ button.py
@@ -2,20 +2,20 @@ import pygame.font
 
 
 class Button:
-    '''A class to build buttons for the game.'''
+    """A class to build buttons for the game."""
 
     def __init__(self, ai_game, msg):
-        '''Initialize button attributes.'''
+        """Initialize button attributes."""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
-        # Set the dimensions and properties of the buitton. 
+        # Set the dimensions and properties of the button.
         self.width, self.height = 200, 50
         self.button_color = (0, 135, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Build the buttons rect object and center it.
+        # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
@@ -23,13 +23,13 @@ class Button:
         self._prep_msg(msg)
 
     def _prep_msg(self, msg):
-        '''Turn msg into a rednered image and center text on the button'''
-        self.msg_image = self.font.render(msg, True, self.text_color, 
-                                            self.button_color)
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
-        '''Draw blank button and then draw message.'''
+        """Draw blank button and then draw message."""
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
