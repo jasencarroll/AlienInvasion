a59566cbfa50a6c47f842ad8af618c34145fa3e2
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-24 06:18:31 -0400

    functional scores, and functional level

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,7 +76,9 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             
             # Reset the game statistics.
-            self.stats.reset_stats()            
+            self.stats.reset_stats()     
+            self.sb.prep_score()    
+            self.sb.prep_level()   
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -131,11 +133,21 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():    
+                self.stats.score =+ self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level. 
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         '''Respond to the ship being hit by an alien.'''
